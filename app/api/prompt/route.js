da2d34e5a486a05ef6e1f3e93c22005ec58b0ddb
Akankhya <dashakankhya92@gmail.com>
2024-06-14 15:46:06 +0530

    Added force-dynamic to fetch prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,19 +3,14 @@ import Prompt from "@models/prompt";
 
 // For GET request in API route
 // Getting all the prompts
+export const dynamic = 'force-dynamic';
 export const GET = async (req, res) => {
     try {
         await connectionToDB();
         const prompts = await Prompt.find({}).populate('creator');
         
         return new Response(JSON.stringify(prompts), {
-            status: 200,
-            headers: {
-                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-                'Pragma': 'no-cache',
-                'Expires': '0',
-                'Surrogate-Control': 'no-store',
-            }
+            status: 200
         });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 });
